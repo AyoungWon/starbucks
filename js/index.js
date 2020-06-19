737c39f577693b5e0d4f11582027ed64dbd244f7
AyoungWon <thwn4750@gmail.com>
2020-06-19 10:16:22 +0900

    press-wrap 동적생성

--- js/index.js
+++ js/index.js
@@ -109,22 +109,26 @@ $(".main-wrap > .bt-next").click(onMainNext);
 
 
 /******************* 슬라이드 직접코딩2 ********************/
-var prdNow = 0,  prdSize = 6, prdLast, prdLeft, prdTar;
-var prds = [], prdArr = [];
+var prdNow = 0,
+	prdSize = 6,
+	prdLast, prdLeft, prdTar;
+var prds = [],
+	prdArr = [];
 $(".prd-wrapper > .bt-left").click(onPrdLeft);
 $(".prd-wrapper > .bt-right").click(onPrdRight);
 
 $.get("../json/prds.json", onPrdLoad);
+
 function onPrdLoad(r) {
 	prdLast = r.prds.length - 1;
 	var html = '';
-	for(var i in r.prds) {
-		html  = '<li class="prd">';
-		html += '	<div class="prd-img"><img src="'+r.prds[i].src+'" class="img"></div>';
-		html += '	<div class="prd-title">'+r.prds[i].title+'</div>';
-		html += '	<div class="prd-price">'+r.prds[i].price+'</div>';
+	for (var i in r.prds) {
+		html = '<li class="prd">';
+		html += '	<div class="prd-img"><img src="' + r.prds[i].src + '" class="img"></div>';
+		html += '	<div class="prd-title">' + r.prds[i].title + '</div>';
+		html += '	<div class="prd-price">' + r.prds[i].price + '</div>';
 		html += '</li>';
-		prds.push($(html));		
+		prds.push($(html));
 	}
 	prdInit();
 }
@@ -133,8 +137,8 @@ function prdInit() {
 	prdArr = [];
 	prdArr[1] = prdNow;
 	prdArr[0] = (prdNow == 0) ? prdLast : prdNow - 1;
-	for(var i=2; i<prdSize; i++) prdArr[i] = (prdArr[i-1] == prdLast) ? 0 : prdArr[i-1] + 1;
-	for(var i=0; i<prdArr.length; i++) $(prds[prdArr[i]]).clone().appendTo(".prd-wrap");
+	for (var i = 2; i < prdSize; i++) prdArr[i] = (prdArr[i - 1] == prdLast) ? 0 : prdArr[i - 1] + 1;
+	for (var i = 0; i < prdArr.length; i++) $(prds[prdArr[i]]).clone().appendTo(".prd-wrap");
 }
 
 function onPrdLeft() {
@@ -150,8 +154,12 @@ function onPrdRight() {
 }
 
 function prdAni() {
-	$(".prd-wrap").stop().animate({"left": prdTar}, 500, function(){
-		$(this).empty().css({"left": prdLeft+"%"});
+	$(".prd-wrap").stop().animate({
+		"left": prdTar
+	}, 500, function () {
+		$(this).empty().css({
+			"left": prdLeft + "%"
+		});
 		prdInit();
 	});
 }
@@ -162,22 +170,22 @@ $.get("../json/locations.json", onLocationLoad);
 function onLocationLoad(r) {
 	var html = '';
 	for (var i in r.locs) {
-		html  = '<li class="store">';
-		html += '<div class="photo"><img src="'+r.locs[i].src+'" class="img"></div>';
-		html += '<p class="cont">'+r.locs[i].cont+'</p>';
+		html = '<li class="store">';
+		html += '<div class="photo"><img src="' + r.locs[i].src + '" class="img"></div>';
+		html += '<p class="cont">' + r.locs[i].cont + '</p>';
 		html += '<div class="addr">';
 		html += '<i class="fa fa-map-marker-alt"></i>';
-		html += '<span class="rc">Address: '+r.locs[i].addr+'</span>';
+		html += '<span class="rc">Address: ' + r.locs[i].addr + '</span>';
 		html += '</div>';
 		html += '<div class="time">';
 		html += '<i class="fa fa-clock"></i>';
-		html += '<span class="rc">Open: '+r.locs[i].time+'</span>';
+		html += '<span class="rc">Open: ' + r.locs[i].time + '</span>';
 		html += '</div>';
 		html += '<div class="tel">';
 		html += '<i class="fa fa-phone"></i>';
-		html += '<span class="rc">Phone: '+r.locs[i].tel+'</span>';
+		html += '<span class="rc">Phone: ' + r.locs[i].tel + '</span>';
 		html += '</div>';
-		html += '<button data-lat="'+r.locs[i].lat+'" data-lon="'+r.locs[i].lon+'" class="bt-map bt-yellow">See on Map</button>';
+		html += '<button data-lat="' + r.locs[i].lat + '" data-lon="' + r.locs[i].lon + '" class="bt-map bt-yellow">See on Map</button>';
 		html += '</li>';
 		$(".store-wrap").append(html);
 	}
@@ -199,18 +207,26 @@ function onModalWrap(e) {
 }
 
 function onMapOpen() {
-	$(".modal-map").css({"display": "flex", "opacity": 0}).stop().animate({"opacity": 1}, 500);
+	$(".modal-map").css({
+		"display": "flex",
+		"opacity": 0
+	}).stop().animate({
+		"opacity": 1
+	}, 500);
 	var lat = $(this).data("lat");
 	var lon = $(this).data("lon");
 	var container = document.getElementById('map');
-	var options = {center: new kakao.maps.LatLng(lat, lon), level: 2};
+	var options = {
+		center: new kakao.maps.LatLng(lat, lon),
+		level: 2
+	};
 	var map = new kakao.maps.Map(container, options);
 
-var markerPosition  = new kakao.maps.LatLng(lat, lon); 
-var marker = new kakao.maps.Marker({
-    position: markerPosition
-});
-marker.setMap(map);
+	var markerPosition = new kakao.maps.LatLng(lat, lon);
+	var marker = new kakao.maps.Marker({
+		position: markerPosition
+	});
+	marker.setMap(map);
 }
 
 function onMapClose() {
@@ -222,7 +238,7 @@ function onMapClose() {
 }
 
 
-/******************* menu 동적 생성 ********************/
+/******************* menu 동적 생성  ********************/
 $.get("../json/menus.json", onMenuLoad);
 
 function onMenuLoad(r) {
@@ -239,41 +255,45 @@ function onMenuLoad(r) {
 
 
 
-/******************* news 동적생성 ********************/
+/******************* news 동적생성 & 슬라이드********************/
 
-var newsNow = 0,  newsSize = 5, newsLast, newsLeft, newsTar;
-var newss = [], newsArr = [];
+var newsNow = 0,
+	newsSize = 5,
+	newsLast, newsLeft, newsTar;
+var newss = [],
+	newsArr = [];
 $(".news-wrapper > .bt-left").click(onNewsLeft);
 $(".news-wrapper > .bt-right").click(onNewsRight);
 
 $.get("../json/news.json", onnewsLoad);
+
 function onnewsLoad(r) {
 	newsLast = r.news.length - 1;
 	var html = '';
-	for(var i in r.news) {
-		html  = '<li class="news">';
+	for (var i in r.news) {
+		html = '<li class="news">';
 		html += '<div class="news-img">';
-		html += '<img src="'+r.news[i].src+'" class="img">';
+		html += '<img src="' + r.news[i].src + '" class="img">';
 		html += '<div class="badge-tag">';
-		for(var j in r.news[i].badge) {
-			html += '<div class="badge">'+r.news[i].badge[j]+'</div>';
+		for (var j in r.news[i].badge) {
+			html += '<div class="badge">' + r.news[i].badge[j] + '</div>';
 		}
 		html += '</div>';
 		html += '<div class="badge-date">';
-		html += '<div class="month">'+moment(r.news[i].date).format('MMM')+'</div>';
-		html += '<div class="day">'+moment(r.news[i].date).format('DD')+'</div>';
+		html += '<div class="month">' + moment(r.news[i].date).format('MMM') + '</div>';
+		html += '<div class="day">' + moment(r.news[i].date).format('DD') + '</div>';
 		html += '</div>';
 		html += '</div>';
-		html += '<div class="news-title">'+r.news[i].title+'</div>';
+		html += '<div class="news-title">' + r.news[i].title + '</div>';
 		html += '<div class="news-tag">';
-		for(var j in r.news[i].tag) {
-			html += '<span class="tag">'+r.news[i].tag[j]+'</span>';
+		for (var j in r.news[i].tag) {
+			html += '<span class="tag">' + r.news[i].tag[j] + '</span>';
 		}
 		html += '</div>';
-		html += '<div class="news-cont">'+r.news[i].cont+'</div>';
+		html += '<div class="news-cont">' + r.news[i].cont + '</div>';
 		html += '<button class="bt-ghost bt-more">Read more <span>▶</span></button>';
 		html += '</li>';
-		newss.push($(html));		
+		newss.push($(html));
 	}
 	newsInit();
 }
@@ -282,8 +302,8 @@ function newsInit() {
 	newsArr = [];
 	newsArr[1] = newsNow;
 	newsArr[0] = (newsNow == 0) ? newsLast : newsNow - 1;
-	for(var i=2; i<newsSize; i++) newsArr[i] = (newsArr[i-1] == newsLast) ? 0 : newsArr[i-1] + 1;
-	for(var i=0; i<newsArr.length; i++) $(newss[newsArr[i]]).clone().appendTo(".news-wrap");
+	for (var i = 2; i < newsSize; i++) newsArr[i] = (newsArr[i - 1] == newsLast) ? 0 : newsArr[i - 1] + 1;
+	for (var i = 0; i < newsArr.length; i++) $(newss[newsArr[i]]).clone().appendTo(".news-wrap");
 }
 
 function onNewsLeft() {
@@ -299,43 +319,70 @@ function onNewsRight() {
 }
 
 function newsAni() {
-	$(".news-wrap").stop().animate({"left": newsTar}, 500, function(){
-		$(this).empty().css({"left": newsLeft+"%"});
+	$(".news-wrap").stop().animate({
+		"left": newsTar
+	}, 500, function () {
+		$(this).empty().css({
+			"left": newsLeft + "%"
+		});
 		newsInit();
 	});
 }
+
+
+/******************* press 동적생성********************/
+$.get("../json/press.json", onPressLoad);
+
+function onPressLoad(r) {
+	var html;
+	for (var i in r.press) {
+		html = '<li class="press">';
+		html += '<div class="logo"><img src="'+r.press[i].logo+'" alt="로고"></div>';
+		html += '<div class="cont">'+r.press[i].content+'</div>';
+		html += '<div class="writer rc">'+r.press[i].writer+'</div>';
+		html += '</li>';
+		$(".press-ul").append(html);
+	}
+}
+
+
+
 /******************* 이벤트 함수 ********************/
 function onResize() {
 	this.wid = $(this).innerWidth();
 	this.hei = $(this).innerHeight();
-	if(wid > 991) {
+	if (wid > 991) {
 		prdLeft = -25;
 		newsLeft = -33.3333;
-	}
-	else if(wid > 767) {
+	} else if (wid > 767) {
 		prdLeft = -33.3333;
 		newsLeft = -50;
-	}
-	else if(wid > 479) {
+	} else if (wid > 479) {
 		prdLeft = -50;
 		newsLeft = -100;
-	}
-	else if(wid <= 479) {
+	} else if (wid <= 479) {
 		prdLeft = -100;
 		newsLeft = -100;
 	}
-	$(".prd-wrap").css("left", prdLeft+"%");
-	$(".news-wrap").css("left", newsLeft+"%");
+	$(".prd-wrap").css("left", prdLeft + "%");
+	$(".news-wrap").css("left", newsLeft + "%");
 }
 
 function onScroll() {
 	// header 위치
 	this.scTop = $(this).scrollTop();
-	if(scTop > hei) {
-		$(".header").css({"top": 0, "bottom": "auto", "position": "fixed"});
-	}
-	else {
-		$(".header").css({"top": "auto", "bottom": 0, "position": "absolute"});
+	if (scTop > hei) {
+		$(".header").css({
+			"top": 0,
+			"bottom": "auto",
+			"position": "fixed"
+		});
+	} else {
+		$(".header").css({
+			"top": "auto",
+			"bottom": 0,
+			"position": "absolute"
+		});
 	}
 
 	// .loc-wrap의 background-position-y 변화
@@ -344,11 +391,22 @@ function onScroll() {
 	var locEnd = locStart + locHei + hei;
 	var locGap = 0;
 	var speed = 400;
-	if(scTop + hei > locStart && scTop + hei < locEnd) {
-		locGap = (speed/2) - Math.round((scTop + hei - locStart) / (locEnd - locStart) * speed);
+	if (scTop + hei > locStart && scTop + hei < locEnd) {
+		locGap = (speed / 2) - Math.round((scTop + hei - locStart) / (locEnd - locStart) * speed);
 		$(".loc-wrap").css("background-position-y", locGap + "%");
 	}
 }
+
+	// .loc-wrap의 background-position-y 변화
+	var pressStart = $(".press-wrap").offset().top;
+	var pressHei = $(".press-wrap").innerHeight();
+	var pressEnd = pressStart + pressHei + hei;
+	var pressGap = 0;
+	var pressSpeed = 200;
+	if (scTop + hei > pressStart && scTop + hei < pressEnd) {
+		pressGap = (speed / 2) - Math.round((scTop + hei - pressStart) / (pressEnd - pressStart) * PressSpeed);
+		$(".press-wrap").css("background-position-y", pressGap + "%");
+	}
 /******************* 이벤트 설정 ********************/
 $(window).resize(onResize).trigger("resize");
 $(window).scroll(onScroll).trigger("scroll");
